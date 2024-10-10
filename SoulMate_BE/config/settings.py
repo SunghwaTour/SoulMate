e63024f755f968d5cf963fcd5c4486fd1ff9781a
yoosuhyeon <dbtngus20@naver.com>
2024-10-10 19:31:26 +0900

    fix : hosts, cors setting

--- SoulMate_BE/config/settings.py
+++ SoulMate_BE/config/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = my_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:8000', 'http://localhost:8000','http://35.192.131.111')
+ALLOWED_HOSTS = ['127.0.0.1', '35.192.131.111']
+CORS_ORIGIN_WHITELIST = 'http://127.0.0.1:8000', 'http://35.192.131.111/:8000'
 CORS_ALLOW_CREDENTIALS = True
 
 pymysql.install_as_MySQLdb()
