db6269adb94db1703ab6d5df9b3d89c4e7ff2f4b
yoosuhyeon <dbtngus20@naver.com>
2024-10-10 19:14:13 +0900

    fix : port number

--- SoulMate_BE/config/settings.py
+++ SoulMate_BE/config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = my_settings.SECRET_KEY
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000','http://35.192.131.111')
+CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:8000', 'http://localhost:8000','http://35.192.131.111')
 CORS_ALLOW_CREDENTIALS = True
 
 pymysql.install_as_MySQLdb()
