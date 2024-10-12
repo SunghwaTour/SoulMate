e9f2c50a77ababc0cc72b52e0e987277fcbf02a6
yoosuhyeon <dbtngus20@naver.com>
2024-10-12 17:22:28 +0900

    fix : cors setting and path setting

--- SoulMate_BE/config/wsgi.py
+++ SoulMate_BE/config/wsgi.py
@@ -7,9 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/home/kingbus/community/SoulMate/SoulMate_BE')
+sys.path.append('/home/kingbus/community/SoulMate/SoulMate_BE/venv/lib/python3.8/site-packages')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
