590b6229fe95bdbca6cffa9d937b1bd40dc09834
yoosuhyeon <dbtngus20@naver.com>
2024-10-13 00:08:58 +0900

    feat : add phone number verification when registering as a member

--- SoulMate_BE/user/serializers.py
+++ SoulMate_BE/user/serializers.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from rest_framework import serializers
 from .models import User
 from django.contrib.auth.hashers import make_password, check_password
+from django.core.cache import cache
 
 class SignUpSerializer(serializers.ModelSerializer):
     password_confirm = serializers.CharField(write_only=True) # 전화번호 확인 필드
@@ -16,6 +17,12 @@ class SignUpSerializer(serializers.ModelSerializer):
         # 비밀번호 일치 확인
         if data['password'] != data['password_confirm']:
             raise serializers.ValidationError({"비밀번호가 일치하지 않습니다."})
+        
+        # 전화번호 인증 확인 (캐시에서 인증 코드가 있는지 확인)
+        phone_number = data.get('phone_number')
+        if cache.get(phone_number) is None:
+            raise serializers.ValidationError({"phone_number": "전화번호 인증이 완료되지 않았습니다."})
+
         return data
 
     # User 생성
