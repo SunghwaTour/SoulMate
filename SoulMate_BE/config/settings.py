0f21d35a0bc8129f23127565f3b04e61e6170e62
yoosuhyeon <dbtngus20@naver.com>
2024-10-18 12:10:59 +0900

    feat : board app

--- SoulMate_BE/config/settings.py
+++ SoulMate_BE/config/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'user',
     'corsheaders',
+    'board',
 ]
 
 REST_FRAMEWORK = {
--- SoulMate_BE/config/urls.py
+++ SoulMate_BE/config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('user/', include('user.urls')),
+    path('board/', include('board.urls')),
 ]
