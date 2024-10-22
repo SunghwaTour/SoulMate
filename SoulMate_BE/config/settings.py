5c7d7ff8013bb2e31527ee554b87b590d17fd09d
yoosuhyeon <dbtngus20@naver.com>
2024-10-23 00:49:54 +0900

    fix : caches setting

--- SoulMate_BE/config/settings.py
+++ SoulMate_BE/config/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = my_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# mysettings.py 파일에서 캐시 설정 가져오기
-from .mysettings import CACHES
+# my_settings.py 파일에서 캐시 설정 가져오기
+from my_settings import CACHES
 
 ALLOWED_HOSTS = ['127.0.0.1', '35.192.131.111', 'kingbus.kr']
 CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:8000', 'http://localhost:3000', 'http://35.192.131.111', 'http://kingbus.kr']
