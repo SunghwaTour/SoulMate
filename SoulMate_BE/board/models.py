22b351b005170e6ae6be05344d5688ea1aaa3b0e
yoosuhyeon <dbtngus20@naver.com>
2024-10-25 20:28:51 +0900

    fix : change model blank option

--- SoulMate_BE/board/models.py
+++ SoulMate_BE/board/models.py
@@ -5,9 +5,9 @@ import uuid
 # Create your models here.
 class Board(models.Model) :
     board_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, null=False, blank=True) # 키값
-    title = models.CharField(max_length=50, null=False, blank=True) # 제목
-    content = models.TextField() # 내용
+    title = models.CharField(max_length=50, null=False) # 제목
+    content = models.TextField(null=False) # 내용
     created_At = models.DateTimeField(auto_now_add=True) # 작성일
     updated_At = models.DateTimeField(auto_now=True) # 수정일
-    user  = models.ForeignKey(User, on_delete=models.CASCADE, blank=True) # 작성자
+    user  = models.ForeignKey(User, on_delete=models.CASCADE, null=False) # 작성자
 
