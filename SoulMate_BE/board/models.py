391b3bf7b44e6963302fd678f3aa630cd378f3b0
yoosuhyeon <dbtngus20@naver.com>
2024-10-28 02:01:31 +0900

    fix : change foreign key option

--- SoulMate_BE/board/models.py
+++ SoulMate_BE/board/models.py
@@ -9,12 +9,12 @@ class Board(models.Model) :
     content = models.TextField(null=False) # 내용
     created_At = models.DateTimeField(auto_now_add=True) # 작성일
     updated_At = models.DateTimeField(auto_now=True) # 수정일
-    user  = models.ForeignKey(User, on_delete=models.CASCADE, null=False) # 작성자
+    user  = models.ForeignKey(User, on_delete=models.CASCADE) # 작성자
 
 class Comment(models.Model) :
     comment_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, null=False)
-    board = models.ForeignKey(Board, on_delete=models.CASCADE, null=False) # 게시판
-    user  = models.ForeignKey(User, on_delete=models.CASCADE, null=False) # 작성자
+    board = models.ForeignKey(Board, on_delete=models.CASCADE) # 게시판
+    user  = models.ForeignKey(User, on_delete=models.CASCADE) # 작성자
     content = models.TextField(null=False) # 댓글 내용
     created_At = models.DateTimeField(auto_now_add=True) # 작성일
     updated_At = models.DateTimeField(auto_now=True) # 수정일
